5cb33310ad31909df64329ddc3b590581f0069ed
boseaditya21 <AdityaBose@21>
2026-04-08 16:08:20 +0530

    Hangman

--- hangman.py
+++ hangman.py
@@ -1,29 +1,30 @@
 import random
 
 word_list = ["aardvark", "baboon", "camel"]
-
 chosen_word = random.choice(word_list)
-print(chosen_word)
-
-placeholder=""
 word_length = len(chosen_word)
-for position in range(word_length):
-    placeholder+= "_"
-print(placeholder)
+
+# Create the initial display (e.g., ["_", "_", "_", "_"])
+display = []
+for _ in range(word_length):
+    display.append("_")
 
 game_over = False
 
-while not game_over: 
-    guess = input("Guess a letter: ").lower();
+while not game_over:
+    guess = input("Guess a letter: ").lower()
 
-    display = ""
-
-    for letter in chosen_word:
+    # Check each letter in the chosen_word
+    for position in range(word_length):
+        letter = chosen_word[position]
         if letter == guess:
-            display += letter
-        else:
-            display += "_"
+            # Update the display at the correct index
+            display[position] = letter
 
-    print(display)
+    # Join the list into a string to show the user
+    print(f"{' '.join(display)}")
 
-    if
+    # Check if there are no more "_" left in display
+    if "_" not in display:
+        game_over = True
+        print("You win!")
